d0b87cc257e77800c12af11f2e351844cfa56a5c
Giovanni Bassi <giggio@giggio.net>
2025-05-28 12:49:34 -0300

    Don't always show tree-sitter install messages
    See related issue:
    https://github.com/nvim-treesitter/nvim-treesitter/issues/7900

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -283,20 +283,39 @@ require("lazy").setup({
         "mfussenegger/nvim-dap",
       },
     },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      branch = 'main',
-      -- event = "VeryLazy",
-      lazy = false,
-      build = ":TSUpdate",
-      config = function()
-        require'nvim-treesitter'.install {
-          'rust',
-          'javascript',
-          'lua'
-        }
-      end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'main',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local languages = {
+        'javascript',
+        'lua',
+        'rust',
+      }
+      local installed = vim.fn.glob(vim.fn.stdpath('data') .. '/site/parser/*.so', 0, 1)
+      local missing = {}
+      for _, lang in ipairs(languages) do
+        local found = false
+        for _, file in ipairs(installed) do
+          if file:match(lang .. '%.so$') then
+            found = true
+            break
+          end
+        end
+        if not found then
+          table.insert(missing, lang)
+        end
+      end
+      if #missing > 0 then
+        print("Missing parsers: " .. table.concat(missing, ", "))
+        require'nvim-treesitter'.install(missing)
+      else
+        print("All tree-sitter parsers are installed.")
+      end
+    end,
+  },
     {
       "rcarriga/nvim-dap-ui",
       event = "VeryLazy",
