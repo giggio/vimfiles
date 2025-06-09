46f32dea4e294a4fc013b778649866e27a6d614c
Giovanni Bassi <giggio@giggio.net>
2025-06-09 20:06:08 -0300

    Remove LSP servers, as Nvim acts a server
    By adding the LSs only in this repo, if nvim is started
    anywhere else it will fail to find them. So they were moved
    to the nix repo.

--- shell.nix
+++ shell.nix
@@ -3,10 +3,6 @@
 pkgs.mkShell {
   name = "vimfiles";
   nativeBuildInputs = with pkgs; [
-    vim-language-server # VImScript language server, LSP for vim script https://github.com/iamcco/vim-language-server
-    lua-language-server # Lua language server https://github.com/LuaLS/lua-language-server
-    # I'd add luajit here, but I'm installing it globally, as I need some packages for shared
-    # extensions, like tiktoken_core, used by Github Copilot Chat.
   ];
   shellHook = ''
     echo "Let's (Neo)Vim!"
