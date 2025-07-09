c50c0ddf052110f07fa9dbb621a8858da4b28cb4
Giovanni Bassi <giggio@giggio.net>
2025-07-09 20:50:39 -0300

    Add Kulala global keymaps

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -19,7 +19,7 @@ return {
     },
     ft = {"http", "rest"},
     opts = {
-      global_keymaps = false,
+      global_keymaps = true,
       global_keymaps_prefix = "<leader>R",
       kulala_keymaps_prefix = "",
     },
