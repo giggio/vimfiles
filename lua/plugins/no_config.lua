868e08e0eb87e150f6b746ede4b1ca844157b1df
Giovanni Bassi <giggio@giggio.net>
2025-10-01 10:59:51 -0300

    Remove lightbulb plugin and update plugins

--- lua/plugins/no_config.lua
+++ lua/plugins/no_config.lua
@@ -7,21 +7,21 @@ return {
     end,
   },
   "JoosepAlviste/nvim-ts-context-commentstring",
-  {
-    "kosayoda/nvim-lightbulb",
-    config = function()
-      require('nvim-lightbulb').setup {
-        autocmd = { enabled = true },
-        action_kinds = {
-          "call_hints",
-          "quickfix",
-          "diagnostic",
-          "fix_all",
-          "inlay_hint",
-        },
-      }
-    end,
-  },
+  -- {
+  --   "kosayoda/nvim-lightbulb",
+  --   config = function()
+  --     require('nvim-lightbulb').setup {
+  --       autocmd = { enabled = true },
+  --       action_kinds = {
+  --         "call_hints",
+  --         "quickfix",
+  --         "diagnostic",
+  --         "fix_all",
+  --         "inlay_hint",
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     "Joakker/lua-json5",
     build = vim.fn.has('unix') and "./install.sh" or "powershell ./install.ps1",
