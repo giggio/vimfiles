ae1f3df274cdb2360ab9a0b2e5cdd207cf506860
Giovanni Bassi <giggio@giggio.net>
2025-10-19 19:55:11 -0300

    Update copilot chat config, set model

--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -1,3 +1,5 @@
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+-- Chat with GitHub Copilot in Neovim
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -9,7 +11,15 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      -- See Configuration section for options
+      {
+        model = 'gpt-4.1',           -- AI model to use
+        temperature = 0.1,           -- Lower = focused, higher = creative
+        window = {
+          layout = 'vertical',       -- 'vertical', 'horizontal', 'float'
+          width = 0.3,              -- 50% of screen width
+        },
+        auto_insert_mode = true,     -- Enter insert mode when opening
+      }
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
