655f796e2ac63981c84027f86f3b4c458c7d6348
Giovanni Bassi <giggio@giggio.net>
2025-11-01 14:33:26 -0300

    Refactor formatting logic to handle redo

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
   lazy = true,
   event = "VeryLazy",
   config = function()
+    local undo_redo_detected = false
     require("conform").setup({
       log_level = vim.log.levels.DEBUG,
       default_format_opts = {
@@ -13,11 +14,16 @@ return {
         lsp_format = "fallback",
       },
       format_on_save = function(bufnr)
-        local errors =
-          vim.diagnostic.get(bufnr, { severity = { min = vim.diagnostic.severity.ERROR } })
+        local errors = vim.diagnostic.get(bufnr, { severity = { min = vim.diagnostic.severity.ERROR } })
         if #errors > 0 then
-          return
+          return { dry_run = true }
         end
+        if undo_redo_detected then
+          -- vim.fn.VerboseEchomsg("Skipping format ON SAVE due to undo/redo")
+          -- print("Skipping format ON SAVE due to undo/redo")
+          return { dry_run = true }
+        end
+        -- print("Formatting ON SAVE due to diagnostics change")
         return {
           timeout_ms = 500,
           lsp_fallback = true,
@@ -50,8 +56,7 @@ return {
       require("conform").format({ async = true, range = range })
     end, { range = true })
 
-    local group_format_when_diags_change =
-      vim.api.nvim_create_augroup("FormatWhenDiagnosticsChange", { clear = true })
+    local group_format_when_diags_change = vim.api.nvim_create_augroup("FormatWhenDiagnosticsChange", { clear = true })
 
     vim.api.nvim_create_autocmd("DiagnosticChanged", {
       group = group_format_when_diags_change,
@@ -60,15 +65,40 @@ return {
         if mode ~= "n" and not mode:match("v") then
           return
         end
-        local errors =
-          vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
+        local errors = vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
         if #errors > 0 then
           return
         end
         if vim.api.nvim_get_option_value("readonly", { buf = args.buf }) ~= "noreadonly" then
           return
         end
+        if undo_redo_detected then
+          -- print("Skipping format due to undo/redo")
+          return
+        end
+        -- print("Formatting due to diagnostics change")
         require("conform").format({ bufnr = args.buf })
+      end,
+    })
+
+    local prev = vim.fn.undotree()
+    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+      group = group_format_when_diags_change,
+      callback = function()
+        local u = vim.fn.undotree()
+        if u.seq_cur < prev.seq_cur then
+          -- print("Undo detected")
+          undo_redo_detected = true
+        elseif u.seq_cur > prev.seq_cur then
+          if u.seq_last == prev.seq_last then
+            -- print("Redo detected")
+            undo_redo_detected = true
+          else
+            undo_redo_detected = false
+            -- print("Edit detected")
+          end
+        end
+        prev = u
       end,
     })
 
@@ -77,8 +107,7 @@ return {
     vim.api.nvim_create_autocmd("InsertEnter", {
       group = group_format_when_diags_change,
       callback = function(args)
-        local errors =
-          vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
+        local errors = vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
         state_buffer_had_errors[args.buf] = #errors > 0
       end,
     })
@@ -87,8 +116,7 @@ return {
       group = group_format_when_diags_change,
       callback = function(args)
         if state_buffer_had_errors[args.buf] then
-          local errors =
-            vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
+          local errors = vim.diagnostic.get(args.buf, { severity = { min = vim.diagnostic.severity.ERROR } })
           if #errors == 0 then
             require("conform").format({ bufnr = args.buf })
           end
