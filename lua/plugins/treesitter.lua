32a8b817ecd85722c8d458e66e29403e8a4f2595
Giovanni Bassi <giggio@giggio.net>
2026-02-01 17:35:07 -0300

    Limit the number of lines to display context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -98,10 +98,10 @@ return {
     require("treesitter-context").setup({
       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
       multiwindow = true, -- Enable multiwindow support.
-      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      max_lines = 7, -- How many lines the window should span. Values <= 0 mean no limit.
       min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
       line_numbers = true,
-      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      multiline_threshold = 3, -- Maximum number of lines to show for a single context
       trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
       mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
       zindex = 20, -- The Z-index of the context window
